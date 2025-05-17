fc952695a5b99379b72c3df86f832eb1c1987404
Amruth Arunkumar <amruth1.618@gmail.com>
2025-05-16 21:58:05 -0400

    connecting backend and frontend

--- part-3/backend/index.js
+++ part-3/backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
 app.use(express.json());
+app.use(express.static("dist"));
+app.use(cors());
 
 let notes = [
     {
@@ -64,12 +69,12 @@ app.post("/api/notes", (request, response) => {
         id: generateId(),
     };
 
-    notes = notes.concat(note)
-    
+    notes = notes.concat(note);
+
     response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
